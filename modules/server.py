f767fbfcdab52a4a7d2a7a3103f8cec6486f22b8
Akash Bhave <akashbbhave@gmail.com>
2019-03-02 17:18:01 -0500

    Add template and JS to server

--- modules/server.py
+++ modules/server.py
@@ -1,9 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+import time
+from threading import Thread
 
-@app.route('/')
-def root():
-    return "HELLO"
-    
+import yaml
+import flask
+
+from main import load_config
+config = load_config()
+
+app = flask.Flask(__name__)
+
+temp = 0
+ 
+@app.route("/")
+def path_home() -> None:
+    return flask.render_template('index.html', temp=temp)
+
+def update_field(field: int) -> None:
+    global temp
+    temp = field
+    print(temp)
+ 
 def start():
-    app.run(debug=True)
+    Thread(target=lambda: app.run(port=config["modules"]["server"]["port"])).start()
+    time.sleep(10)
+    update_field(10)
