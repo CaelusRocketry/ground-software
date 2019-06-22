b27f61e49e3a1e96303a04932fb0321f1103d73c
Srikar Gouru <srikarg89@gmail.com>
2019-06-22 19:58:20 -0400

    Python ground telemetry

---
+++ telem_test.py
@@ -0,0 +1,22 @@
+import socket
+import threading
+
+IP = '127.0.0.1'
+PORT = 8081
+
+def start():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((IP, PORT))
+    sock.listen(1)
+    conn, addr = sock.accept()
+    listen_thread = threading.Thread(target=listen, args=(conn,))
+    listen_thread.start()
+    
+def listen(conn):
+    while True:
+        data = conn.recv(1024)
+        data = data.decode()
+        print(data)
+
+if __name__ == "__main__":
+    start()
