f0123b8277f542735a352b258bbfea60d9a25ad4
Srikar Gouru <srikarg89@gmail.com>
2019-08-25 12:19:47 -0400

    Reformatted telmetry and packet

--- modules/telemetry.py
+++ modules/telemetry.py
@@ -8,24 +8,27 @@ from packet import Packet
 from Crypto.Cipher import PKCS1_OAEP
 import ast
 
-FLIGHT_IP = '127.0.0.1'
-FLIGHT_PORT = 5005
+GS_IP = '127.0.0.1'
+GS_PORT = 5005
 BYTE_SIZE = 8192
 
 DELAY = .05
 DELAY_LISTEN=.05
 DELAY_SEND=.05
+DELAY_HEARTBEAT=.5
 
 SEND_ALLOWED=True
 
 queue_send=[]
 
 def create_socket():
+    print("Creating socket")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((FLIGHT_IP, FLIGHT_PORT))
+    sock.bind((GS_IP, GS_PORT))
     sock.listen(1)
     conn, addr = sock.accept()
+    print("Created socket")
     return conn
 
 def send(sock):
@@ -39,6 +42,7 @@ def listen(sock):
     while True:
         data = sock.recv(BYTE_SIZE)
         ingest_thread = threading.Thread(target=ingest, args=(data,))
+        ingest_thread.daemon = True
         ingest_thread.start()
         time.sleep(DELAY_LISTEN)
 
@@ -49,7 +53,7 @@ def enqueue(packet = Packet()):
 
 def ingest(encoded):
     packet_str = decode(encoded)
-    packet = Packet.from_string(Packet.from_string(packet_str))
+    packet = Packet.from_string(packet_str)
     print("Incoming: "+packet.message)
 
 def encode(packet):
@@ -64,13 +68,23 @@ def decode(message):
     cipher = PKCS1_OAEP.new(key)
     return cipher.decrypt(ast.literal_eval(str(message))).decode("utf-8")
 
+def heartbeat():
+    while True:
+        enqueue(Packet(header="HEARTBEAT", message="AT"))
+        time.sleep(DELAY_HEARTBEAT)
+
 if __name__ == "__main__":
     sock = create_socket()
     send_thread = threading.Thread(target=send, args=(sock,))
+    send_thread.daemon = True
     listen_thread = threading.Thread(target=listen, args=(sock,))
     listen_thread.daemon = True
     send_thread.start()
     listen_thread.start()
     print("Listening and sending")
+    heartbeat_thread = threading.Thread(target=heartbeat)
+    heartbeat_thread.daemon = True
+    heartbeat_thread.start()
     while(True):
-        enqueue(Packet(message=input("")))
+        temp = input("").split(" ")
+        enqueue(Packet(header=temp[0], message=temp[1]))
