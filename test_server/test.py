c7ccfecabb3d3ffcb2c076f5bf9c76616cb86dae
Akash Bhave <akashbbhave@gmail.com>
2019-11-09 18:16:07 -0500

    Delete all files

--- test_server/test.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(("localhost", 8080))
-sock.listen(1)
-conn, addr = sock.accept()
-print(conn.recv(1024))
-conn.send("Wassup?".encode())
