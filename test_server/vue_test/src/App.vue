a98b5dd6e8697fb26221da13985dfb23c5b57cc8
Srikar Gouru <srikarg89@gmail.com>
2019-11-09 17:50:43 -0500

    basic GUI

---
+++ test_server/vue_test/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div id="heading">
+      <img id="logo" alt="Vue logo" src="./assets/logo.png">
+    </div>
+    <div id="base">
+      <Base/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Base from './components/Base.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Base
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+#heading {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 0px;
+  max-height: 100px;
+}
+#logo {
+   max-height: 150px;
+}
+#base {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
