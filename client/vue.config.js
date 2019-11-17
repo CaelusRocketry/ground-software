5fd57b2e426ffe353d1c62b43ebed141931c74c9
Akash Bhave <akashbbhave@gmail.com>
2019-11-17 08:50:18 -0500

    Create working Flask to Vue websocket using Socket.IO

---
+++ client/vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    outputDir: '../server/dist',
+    // relative to outputDir
+    assetsDir: 'static'
+};
---
+++ server/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+# Initialize Flask
+# Use a custom static/template folder location, Vue builds to here
+app = Flask(__name__,
+            static_folder="./dist/static",
+            template_folder="./dist")
+socketio = SocketIO(app)
+
+
+@socketio.on('test')
+def handle_test(data):
+    print("server: recv 'test'")
+
+
+@app.route("/")
+def handle_root():
+    """
+    Render the built Vue site.
+    """
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
