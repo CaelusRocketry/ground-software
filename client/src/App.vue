f9ac031b4182421e2d43584b38e4f35f84f80afb
Akash Bhave <akashbbhave@gmail.com>
2019-11-17 10:12:28 -0500

    Update favicon, copy assets from ground-gui-demo

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,12 +6,51 @@
 
 <script>
 export default {
-    name: "app",
+    name: 'app',
     methods: {
         send: function() {
             console.log("client: emit 'test'");
-            this.$socket.emit("test", {});
+            this.$socket.emit('test', {});
         }
     }
 };
 </script>
+
+<style>
+@font-face {
+    font-family: 'Karla';
+    src: url('~@/assets/ttf/Karla-Regular.ttf');
+}
+@font-face {
+    font-family: 'Space Mono';
+    src: url('~@/assets/ttf/SpaceMono-Regular.ttf');
+}
+html,
+body {
+    color: #0d1321 !important;
+    background-color: #f3f7f0;
+    overflow-x: hidden;
+    font-family: 'Karla', Helvetica, Arial, sans-serif !important;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+#app {
+    overflow: hidden;
+}
+code,
+pre {
+    font-family: 'Space Mono';
+}
+.align-left {
+    text-align: left;
+}
+.align-center {
+    text-align: center;
+}
+.align-right {
+    text-align: right;
+}
+.buffer-bottom {
+    margin-bottom: 3vh !important;
+}
+</style>
