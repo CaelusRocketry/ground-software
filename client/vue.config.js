4940bbd85b25f97e0c93d4a9e1eda9216ff20662
Akash Bhave <akashbbhave@gmail.com>
2019-11-17 08:58:54 -0500

    Change client port to 5005 and allow CORS

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     outputDir: '../server/dist',
-    // relative to outputDir
-    assetsDir: 'static'
+    assetsDir: 'static', // relative to outputDir
+
+    devServer: {
+        port: 5005 // Close to the server port (5000)
+    }
 };
