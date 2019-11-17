ee611c44b8da3d1f478b6d30240ce7402df16f53
Akash Bhave <akashbbhave@gmail.com>
2019-11-17 12:02:32 -0500

    Create all components and arrange in a grid

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,56 +1,80 @@
 <template>
     <div id="app">
-        <button @click="send">SEND</button>
+        <PaneMessages />
+        <div style="display: grid; grid-template-rows: 1fr 1fr;">
+            <PaneGraph />
+            <PaneGraph />
+        </div>
+        <div style="display: grid; grid-template-rows: 100px auto;">
+            <PaneInfo />
+            <PaneButtons />
+        </div>
     </div>
 </template>
 
 <script>
+import PaneMessages from "@/components/PaneMessages.vue";
+import PaneGraph from "@/components/PaneGraph.vue";
+import PaneInfo from "@/components/PaneInfo.vue";
+import PaneButtons from "@/components/PaneButtons.vue";
+
 export default {
-    name: 'app',
-    methods: {
-        send: function() {
-            console.log("client: emit 'test'");
-            this.$socket.emit('test', {});
-        }
+    name: "app",
+    components: {
+        PaneMessages,
+        PaneGraph,
+        PaneInfo,
+        PaneButtons
     }
 };
 </script>
 
 <style>
 @font-face {
-    font-family: 'Karla';
-    src: url('~@/assets/ttf/Karla-Regular.ttf');
+    font-family: "Karla";
+    src: url("~@/assets/ttf/Karla-Regular.ttf");
 }
 @font-face {
-    font-family: 'Space Mono';
-    src: url('~@/assets/ttf/SpaceMono-Regular.ttf');
+    font-family: "Space Mono";
+    src: url("~@/assets/ttf/SpaceMono-Regular.ttf");
 }
-html,
+
+html {
+    height: 100vh;
+}
+
 body {
-    color: #0d1321 !important;
+    margin: 10px;
+    height: calc(100vh - 20px);
     background-color: #f3f7f0;
     overflow-x: hidden;
-    font-family: 'Karla', Helvetica, Arial, sans-serif !important;
+    max-height: 100vh;
+    max-width: 100vw;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
 }
-#app {
-    overflow: hidden;
+
+body * {
+    grid-gap: 10px;
 }
+
+#app {
+    height: 100%;
+    overflow: hidden;
+    color: #0d1321 !important;
+    font-family: "Karla", Helvetica, Arial, sans-serif !important;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 10px;
+}
+
+.pane {
+    border: 1px solid #0d1321;
+}
+
 code,
 pre {
-    font-family: 'Space Mono';
-}
-.align-left {
-    text-align: left;
-}
-.align-center {
-    text-align: center;
-}
-.align-right {
-    text-align: right;
-}
-.buffer-bottom {
-    margin-bottom: 3vh !important;
+    font-family: "Space Mono";
 }
 </style>
---
+++ client/src/components/PaneInfo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="pane-info">
+        <div style="justify-self: start;">
+            <h1>Project Caelus</h1>
+            <p>Ground Software</p>
+        </div>
+        <img style="justify-self: end;" src="@/assets/img/banner.png" />
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.pane-info {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    padding: 10px;
+}
+.pane-info h1 {
+    margin: 0 0 10px 0;
+    font-size: 1.8em;
+}
+.pane-info p {
+    margin: 0;
+}
+
+img {
+    max-height: 100%;
+}
+</style>
