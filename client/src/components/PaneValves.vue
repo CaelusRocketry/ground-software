272514e48400d19a2b75b32fe49ba6d6a9a5a852
Srikar Gouru <srikarg89@gmail.com>
2019-12-01 12:46:08 -0500

    Split buttons sectoins into valves and abort sections

---
+++ client/src/components/PaneValves.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="pane pane-valve" style="display: grid; grid-template-rows: 100px auto;">Valves
+        <div>
+        </div>
+        <div style="display: grid; grid-template-rows: 25px auto;">
+            <div id="valve_actuate_options" style="margin-left: 15px;">
+                Valve:
+                <select id="valve_select" style="margin-right: 10px;">
+                    <option value=1>Oxidizer Pressure Relief Valve</option>
+                    <option value=2>Fuel Pressure Relief Valve</option>
+                    <option value=3>Fuel to Engine Valve</option>
+                    <option value=4>Oxidizer to Engine Valve</option>
+                    <option value=5>Fuel Outflow Valve</option>
+                    <option value=6>Oxidizer Outflow Valve</option>
+                </select>
+                Priority:
+                <select id="priority_select">
+                    <option value=1>1</option>
+                    <option value=2>2</option>
+                    <option value=3>3</option>
+                    <option value=4>4</option>
+                    <option value=5>5</option>
+                    <option value=6>6</option>
+                </select>
+            </div>
+            <button v-on:click=actuate_valve id="valve_actuate_button">Actuate valve</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "valves",
+    methods: {
+        actuate_valve: function(event){
+            let valve_num = document.getElementById("valve_select").value;
+            let priority = document.getElementById("priority_select").value;
+            alert('Actuating valve ' + valve_num + ' with priority ' + priority);
+            // TODO: Send valve actuation function to backend, which should then send that over telem to the Pi
+        }
+    }
+};
+</script>
--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+#import socket
 
 # Initialize Flask
 # Use a custom static/template folder location, Vue builds to here
@@ -7,7 +8,6 @@ app = Flask(__name__,
             static_folder="./dist/static",
             template_folder="./dist")
 socketio = SocketIO(app, cors_allowed_origins="http://localhost:5005")
-
 
 @socketio.on('test')
 def handle_test(data):
@@ -20,6 +20,9 @@ def handle_root():
     Render the built Vue site.
     """
     return render_template("index.html")
+
+#def actuate_valve(id: int, degree: int, priority: int):
+#    pass
 
 
 if __name__ == '__main__':
