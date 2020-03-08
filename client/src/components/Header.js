6c20d615598eb5db6902001a4632ea7041ba70ee
Akash Bhave <akashbbhave@gmail.com>
2020-03-07 21:16:53 -0500

    Copy and modify timeline component, move header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = ({ title }) => <h1 className="text-2xl font-bold mb-2">{title}</h1>;
+
+export default Header;
--- client/src/panes/PaneHeader.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const PaneHeader = ({ title }) => <h1 className="text-2xl font-bold mb-2">{title}</h1>;
-
-export default PaneHeader;
