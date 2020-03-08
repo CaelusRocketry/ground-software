439604d79517a87e64413aa2a24d61b0b09c3580
Akash Bhave <akashbbhave@gmail.com>
2020-03-07 19:46:48 -0500

    Initialize React project in /client

--- client/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    outputDir: '../server/dist',
-    assetsDir: 'static', // relative to outputDir
-
-    devServer: {
-        port: 5005 // Close to the server port (5000)
-    }
-};
