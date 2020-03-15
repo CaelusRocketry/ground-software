ecebc4a48c6d5aa57020d02c55b158cfc08335cb
Srikar Gouru <srikarg89@gmail.com>
2020-03-15 12:45:48 -0400

    Stylized titles work

--- client/src/panes/SequencePane.js
+++ client/src/panes/SequencePane.js
@@ -7,7 +7,7 @@ const SequencePane = () => (
     <div className="pane">
         <Header title="Sequence" />
         <Timeline>
-            <Event interval="Pending" title={<h1 className="text-xl">Propellant Loading</h1>}>
+            <Event interval="Pending" title={"Propellant Loading"} >
                 <ol>
                     <li>1. Assemble test stand, prepare propellants, etc. </li>
                     <li>2. Ensure all solenoid valves are closed.</li>
@@ -21,14 +21,14 @@ const SequencePane = () => (
                     <li>6. Ensure all ball manual valves are closed</li>
                 </ol>
             </Event>
-            <Event interval="Pending" title={<h1 className="text-xl">Leak Testing Phase 1</h1>}>
+            <Event interval="Pending" title={"Leak Testing Phase 1"}>
                 <ol>
                     <li>1. Return to safe location</li>
                     <li>2. Leave ethanol tank on test stand for 10 minutes</li>
                     <li>3. Ensure propellant mass does not decrease</li>
                 </ol>
             </Event>
-            <Event interval="Pending" title={<h1 className="text-xl">Pressurant Loading</h1>}>
+            <Event interval="Pending" title={"Pressurant Loading"}>
                 <ol>
                     <li>1. Open solenoid valve between N2 tank and ethanol tank</li>
                     <li>2. Close valve once tank pressure reaches 800 psi (monitor pressure)</li>
@@ -36,20 +36,20 @@ const SequencePane = () => (
                     <li>4. Close valve once tank volume reaches proper amount</li>
                 </ol>
             </Event>
-            <Event interval="Pending" title={<h1 className="text-xl">Leak Testing Phase 2</h1>}>
+            <Event interval="Pending" title={"Leak Testing Phase 2"}>
                 <ol>
                     <li>1. Leave tanks on test stand for 30 minutes</li>
                     <li>2. Ensure neither pressure nor mass decreases for either tank</li>
                     <li>a. Monitor temperature, pressure, and mass data for any anomalies</li>
                 </ol>
             </Event>
-            <Event interval="Pending" title={<h1 className="text-xl">Pre-Ignition</h1>}>
+            <Event interval="Pending" title={"Pre-Ignition"}>
                 <ol>
                     <li>1. Item 1</li>
                     <li>2. Item 2</li>
                 </ol>
             </Event>
-            <Event interval="Pending" title={<h1 className="text-xl">Disconnection</h1>}>
+            <Event interval="Pending" title={"Disconnection"}>
                 <ol>
                     <li>1. Item 1</li>
                     <li>2. Item 2</li>
