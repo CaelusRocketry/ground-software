14b5f4dc1a84c367ce244446c2a09294a0d237de
Srikar Gouru <srikarg89@gmail.com>
2020-03-19 11:49:31 -0400

    Create view dropdown button, work on updating view

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,16 @@ import './App.css';
 import StatisticsView from './views/StatisticsView';
 import ActionsView from './views/ActionsView'
 
+let selected_view = "statistics";
 function App() {
+
     return (
         <div className="App font-sans bg-light-1 text-dark-1">
-            {/* <StatisticsView /> */}
-            <ActionsView />
+            <select onChange={(event) => selected_view = event.target.value}>
+                <option value="statistics">Statistics</option>
+                <option value="actions">Actions</option>
+            </select>
+            {selected_view === "statistics" ? <StatisticsView /> : <ActionsView />}
         </div>
     );
 }
