2aa4df9e3d26b9fffe1a463ba8bbea07ea0d6598
Akash Bhave <akashbbhave@gmail.com>
2020-03-19 13:57:26 -0400

    Improve splitting of graph components

--- client/src/panes/GraphPane.js
+++ client/src/panes/GraphPane.js
@@ -1,14 +1,29 @@
 import React from "react";
+import SplitPane from "react-split-pane";
+
+import Header from "../components/Header";
 import Graph from "../components/Graph.js";
+
 import "../index.css";
 
 // used https://openclassrooms.com/en/courses/4286486-build-web-apps-with-reactjs/4286711-build-a-ticking-clock-component
 //blog.cloudboost.io/for-loops-in-react-render-no-you-didnt-6c9f4aa73778
 
 const GraphPane = () => (
-  <div className="pane">
-    <Graph dataType="temperature"></Graph>
-  </div>
+  <SplitPane split="horizontal" className="pane" size="10%" primary="first">
+    <Header title="Graphs" />
+    <div className="h-full border-2">
+      <SplitPane
+        className="view"
+        split="horizontal"
+        size="20%"
+        primary="second"
+      >
+        <Graph dataType="temperature" />
+        <Graph dataType="pressure" />
+      </SplitPane>
+    </div>
+  </SplitPane>
 );
 
 export default GraphPane;
