74000f3be42ed387da301c007e98ce835f652d58
Mitali <mitali.gandhe@gmail.com>
2020-03-22 13:37:27 -0400

    Outline redux store for sensor data

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,21 @@
 import io from 'socket.io-client';
+import { updateLoadData } from './store/actions';
 
 const socket = io('http://localhost:5000');
 
 const socketConnection = (store) => {
     socket.on('heartbeat', function(data){ console.log(data)});
-    socket.on('update sensor data', function(data){ console.log(data)});
+    socket.on('update sensor data', function(data){ 
+
+        // split here? - not in correct order
+
+        dispatchEvent(updateThermocoupleData(data.thermocouple));
+        dispatchEvent(updatePressureData(data.pressure));
+        dispatchEvent(updateLoadData(data.load));
+
+
+
+        console.log(data)});
     socket.on('update valve data', function(data){ console.log(data)});
 }
 
--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,9 +1,32 @@
-export const increment = n => ({
-  type: "INCREMENT",
-  payload: n,
-});
 
-export const decrement = n => ({
-  type: "DECREMENT",
-  payload: n,
-});
+export const updateTermocoupleData = (data) => ({
+  type: "UPDATE_THERMOCOUPLE_DATA",
+  data
+})
+
+export const updatePressureData = (data) => ({
+  type: "UPDATE_PRESSURE_DATA", 
+  data
+})
+
+export const updateLoadData = (data) => ({
+  type: "UPDATE_LOAD_DATA", 
+  data
+})
+
+
+// export const updateSensorData = (data) => ({
+//     type: "UPDATE_SENSOR_DATA", 
+//     data
+// })
+
+
+// export const increment = n => ({
+//   type: "INCREMENT",
+//   payload: n,
+// });
+
+// export const decrement = n => ({
+//   type: "DECREMENT",
+//   payload: n,
+// });
