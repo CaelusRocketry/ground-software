558ba792b8b3f5ff7575b55d24abc6afd9f97f09
Mitali <mitali.gandhe@gmail.com>
2020-03-24 15:47:27 -0400

    Add valve and heartbeat data to store

--- client/src/api.js
+++ client/src/api.js
@@ -1,18 +1,31 @@
 import io from 'socket.io-client';
-import { updateSensorData } from './store/actions';
+import { updateSensorData, updateValveData, updateHeartbeat } from './store/actions';
 
 
 const socket = io('http://localhost:5000');
 
 const socketConnection = (store) => {
-    socket.on('heartbeat', function(data){ console.log(data); });
+    socket.on('heartbeat', function(log){ 
+        // console.log(log)
+
+        store.dispatch(updateHeartbeat(log));
+        console.log(store.getState());
+
+    
+    });
     socket.on('update sensor data', function(log){ 
         // console.log(log)
 
         store.dispatch(updateSensorData(log));
-        console.log(store.getState());
+        // console.log(store.getState());
     });
-    socket.on('update valve data', function(data){ console.log(data); });
+
+    socket.on('update valve data', function(log){ 
+        // console.log(log)
+
+        store.dispatch(updateValveData(log));
+        // console.log(store.getState());
+    });
 }
 
 export {socketConnection}
