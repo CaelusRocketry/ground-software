acb7afd5955f3fbb889932ee37a4ff9073cb156b
Mitali <mitali.gandhe@gmail.com>
2020-03-24 15:04:44 -0400

    Consolidate sensor data transfer to store

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,40 +1,4 @@
-
-export const updateThermocoupleData = (data) => ({
-  type: "UPDATE_THERMOCOUPLE_DATA",
-  state: false,
-  data
+export const updateSensorData = (data) => ({
+    type: "UPDATE_SENSOR_DATA", 
+    data
 })
-
-export const updatePressureData = (data) => ({
-  type: "UPDATE_PRESSURE_DATA",
-  state: false,
-  data
-})
-
-export const updateLoadData = (data) => ({
-  type: "UPDATE_LOAD_DATA", 
-  state: false,
-  data
-})
-
-export const updateTimestamp = (time) => ({
-  type: "UPDATE_TIMESTAMP",
-  time
-})
-
-
-// export const updateSensorData = (data) => ({
-//     type: "UPDATE_SENSOR_DATA", 
-//     data
-// })
-
-
-// export const increment = n => ({
-//   type: "INCREMENT",
-//   payload: n,
-// });
-
-// export const decrement = n => ({
-//   type: "DECREMENT",
-//   payload: n,
-// });
