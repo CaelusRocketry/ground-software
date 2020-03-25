30235e936fa346d4f84d9dcba7e639293ba1e851
Aarav <akhanna@Khannas-MBP-7.fios-router.home>
2020-03-25 16:21:50 -0400

    Add Data Pane and additional visual elements

--- client/src/panes/DataPane.js
+++ client/src/panes/DataPane.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import SplitPane from "react-split-pane";
+import Data from "../components/Data";
+import Header from "../components/Header";
+import "../index.css";
+
+const DataPane = () => (
+    <div>
+      <Header title="Data" />
+      <div className="view">
+          <Data/>
+      </div>
+    </div>
+  );
+  
+  export default DataPane;
