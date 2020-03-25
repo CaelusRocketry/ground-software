bd78056c0cba014236240314a45976b39aab636e
Srikar Gouru <srikarg89@gmail.com>
2020-03-25 18:39:41 -0400

    Update graph dropdown options

--- client/src/views/StatisticsView.js
+++ client/src/views/StatisticsView.js
@@ -6,7 +6,7 @@ import DataPane from "../panes/DataPane";
 import Clock from "../components/Clock";
 
 const StatisticsView = () => (
-  <SplitPane className="view" split="vertical" defaultSize="40%">
+  <SplitPane className="view" split="vertical" defaultSize="20%">
     <SequencePane />
     <SplitPane className="view2" split="vertical" defaultSize="33%">
       <SplitPane className="view3" split="horizontal" defaultSize="40%">
