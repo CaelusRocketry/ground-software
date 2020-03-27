90d560fa21b2f9a450b78cc044fbfac36c47bf28
Nurdin <nurdinhossain@gmail.com>
2020-03-27 00:06:26 -0400

    Implement function to change an object into an array

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useSelector } from "react-redux";
 
 const Message = (props) => {
-    const [messages, updateMessages] = useState([
+    const [messages, updateMessages] = useState(
         {
         header: "sensor_data", 
         message: {
@@ -11,22 +11,39 @@ const Message = (props) => {
             "value": (147, 152)
         }, 
         timestamp: 42
-    }]);
+    });
 
     const style = {
         background: "#F5F5F5",
         padding: "5px",
         marginLeft: "35px"
     };
+
+    const toArray = (obj) => {
+        let arr = Object.entries(obj);
+        
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i][0] == "message") {
+                arr[i][1] = Object.entries(arr[i][1]);
+            }
+        }
+
+        console.log(arr)
+        return arr;
+    };
     return (
         <div>
             <body>
                 <ol className="list-group" style={style}>
-                    {messages.map(message => (
-                        <li key={message} className="list-group-item list-group-item-primary">
-                            {message}
+                        <li>
+                            {toArray(messages)[0][0]}<br/>
+                            {toArray(messages)[0][1]}<br/>
+                            {toArray(messages)[1][0]}:<br/>
+                            {toArray(messages)[1][1][0][0]}: {toArray(messages)[1][1][0][1]}<br/>
+                            {toArray(messages)[1][1][1][0]}: {toArray(messages)[1][1][1][1]}<br/>
+                            {toArray(messages)[1][1][2][0]}: {toArray(messages)[1][1][2][1]}<br/>
+                            {toArray(messages)[2]}
                         </li>
-                    ))}
                 </ol>
             </body>
         </div>
