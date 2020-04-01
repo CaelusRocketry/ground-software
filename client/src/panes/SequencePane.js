fcf44b0fa0f32486a26fbd3477fa1cfe77fb6bed
anna catalano <catalanoanna00@gmail.com>
2020-04-01 16:16:36 -0400

    Implement redux for propellant stage

--- client/src/panes/SequencePane.js
+++ client/src/panes/SequencePane.js
@@ -5,21 +5,45 @@ import Progress from "./../components/Progress";
 import { useDispatch, useSelector } from 'react-redux';
 import { currentStage } from "./../store/actions/index";
 import { Timeline, Event } from "../components/Timeline";
+import { data } from "./../store/reducers/stats";
 
 const SequencePane = () => {
   const dispatch = useDispatch();
-  const current = useSelector(state => state.stages);
-  const final_pressure = 800; // psi
-  const current_pressure = 600; // random value
+  const current = useSelector(state => state.stage);
+  const percentage_pressure = useSelector(state => state.data.general.percent_data);
+  const calcColor = (num) => {
+    if (current === num) {
+      return "#FFFF00"; // yellow
+    }
+    else if (current > num) {
+      return "#00ff00"; // green
+    }
+    else {
+      return "#ff0000"; // red
+    }
+  }; 
+
+  const calcTitle = (num) => {
+    if (current === num) {
+      return "In Progress";
+    }
+    else if (current > num) {
+      return "Completed";
+    }
+    else {
+      return "Pending";
+    }
+  }; 
+
   return (
     <div className="pane">
       <Header title="Sequence" />
       <Timeline>
-        <Event interval={ (current > 0) ? "Completed" : "Pending"} title={"Propellant Loading"} intervalBackground={(current > 0) ? "#00ff00" : "#ff0000"}>
+        <Event interval={ calcTitle(0)} title={"Propellant Loading"} intervalBackground={calcColor(0)}>
           <p1>Propellant loaded?  </p1>
           <button onClick={() => dispatch(currentStage())}>Finished</button>
         </Event>
-        <Event interval={(current > 1) ? "Completed" : "Pending"} title={"Leak Testing Phase 1"} intervalBackground={(current > 1) ? "#00ff00" : "#ff0000"}>
+        <Event interval={calcTitle(1)} title={"Leak Testing Phase 1"} intervalBackground={calcColor(1)}>
           <p1>Time left:  </p1>
           <Timer initialTime={600000} direction="backward" startImmediately={false} checkpoints={[{ time: 0, callback: () => dispatch(currentStage())}]}>
             { ( { start, getTime } ) => (
@@ -32,12 +56,11 @@ const SequencePane = () => {
             ) }
           </Timer>
         </Event>
-        <Event interval={(current > 2) ? "Completed" : "Pending"} title={"Pressurant Loading"} intervalBackground={(current > 2) ? "#00ff00" : "#ff0000"}>
-          <p1>Current Pressure: {current_pressure} psi</p1>
-          <Progress percentage={"" + (current_pressure/final_pressure)*100}/>
+        <Event interval={calcTitle(2)} title={"Pressurant Loading"} intervalBackground={calcColor(2)}>
+          <Progress percentage={"" + percentage_pressure*100}/>
           <button onClick={() => dispatch(currentStage())}>Finished</button>
         </Event>
-        <Event interval={(current > 3) ? "Completed" : "Pending"} title={"Leak Testing Phase 2"} intervalBackground={(current > 3) ? "#00ff00" : "#ff0000"}>
+        <Event interval={calcTitle(3)} title={"Leak Testing Phase 2"} intervalBackground={calcColor(3)}>
         <p1>Time left:  </p1>
           <Timer initialTime={1800000} direction="backward" startImmediately={false} checkpoints={[{ time: 0, callback: () => dispatch(currentStage())}]}  >
             { ( { start, getTime } ) => (
@@ -50,11 +73,13 @@ const SequencePane = () => {
             ) }
           </Timer>
         </Event>
-        <Event interval={(current > 4) ? "Completed" : "Pending"} title={"Pre-Ignition"} intervalBackground={(current > 4) ? "#00ff00" : "#ff0000"}>
+        <Event interval={calcTitle(4)} title={"Pre-Ignition"} intervalBackground={calcColor(4)}>
           <Progress percentage={(current > 4) ? "100" : "0"}/>
+          <button onClick={() => dispatch(currentStage())}>Finished</button>
         </Event>
-        <Event interval={(current > 5) ? "Completed" : "Pending"} title={"Disconnection"} intervalBackground={(current > 5) ? "#00ff00" : "#ff0000"}>
+        <Event interval={calcTitle(5)} title={"Disconnection"} intervalBackground={calcColor(5)}>
           <Progress percentage={(current > 5) ? "100" : "0"}/>
+          <button onClick={() => dispatch(currentStage())}>Finished</button>
         </Event>
       </Timeline>
     </div>
--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from "redux";
 
 import buttonReducer from "./buttons";
 import dataReducer from "./stats";
+import stageReducer from "./stages"
 
-export default combineReducers({ data: dataReducer, buttonReducer: buttonReducer });
+export default combineReducers({ data: dataReducer, buttonReducer: buttonReducer, stage: stageReducer });
