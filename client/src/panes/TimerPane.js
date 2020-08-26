fae7a093b2813762ccfc2a7e53b4d66b63a1a8d8
Nurdin <nurdinhossain@gmail.com>
2020-08-26 16:53:51 -0400

    Only allow countdown to launch if stage is autosequence

--- client/src/panes/TimerPane.js
+++ client/src/panes/TimerPane.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from "react-redux";
 
 function TimerPane() {
     const [seconds, setSeconds] = React.useState(10);
-  
-    React.useEffect(() => {
+    const data = useSelector((state) => (state.data));  
+
+    if (data.general.stage == "autosequence") {
       if (seconds > 0) {
         setTimeout(() => setSeconds(seconds - 1), 1000);
-      } else {
-        setSeconds('0');
       }
-    });
+    }
   
     return (
       <div className="app">
