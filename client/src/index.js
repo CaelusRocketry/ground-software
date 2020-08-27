12571dd883d21836c3be5d5effdac55ae9ca8468
Nurdin <nurdinhossain@gmail.com>
2020-08-26 20:05:47 -0400

    Create global countdown timer through Redux (current problem - display does not update unless page is refereshed)

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,25 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import { createStore } from "redux";
-import { Provider } from "react-redux";
+import { Provider, useSelector, useDispatch } from "react-redux";
 import reducers from "./store/reducers";
 
 import {socketConnection, heartbeatError} from './api';
 
 import * as serviceWorker from "./serviceWorker";
+import { updateCountdown } from "./store/actions";
 
 const store = createStore(reducers);
 socketConnection(store);
 setInterval(() => {console.log(store.getState());}, 1000);
 setInterval(() => {heartbeatError(store)} , 5000);
+if (store.getState().data.general.stage == "autosequence") {
+  setInterval(() => {
+    if (store.getState().data.general.countdown > 0) {
+      store.dispatch(updateCountdown());
+    }
+  }, 1000);
+}
 
 ReactDOM.render(
   <Provider store={store}>
--- client/src/panes/TimerPane.js
+++ client/src/panes/TimerPane.js
@@ -2,19 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from "react-redux";
 
 function TimerPane() {
-    const [seconds, setSeconds] = React.useState(10);
     const data = useSelector((state) => (state.data));  
 
-    if (data.general.stage == "autosequence") {
-      if (seconds > 0) {
-        setTimeout(() => setSeconds(seconds - 1), 1000);
-      }
-    }
-  
     return (
       <div className="app">
         <div className="time">
-          {"T-" + seconds}
+          {"T-" + data.general.countdown}
         </div>
       </div>
     );
