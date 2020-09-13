3711286cbdf1903c7829fab3472ada2c722be27f
Akash Bhave <akashbbhave@gmail.com>
2020-09-13 13:09:28 -0400

    Use monospaced font for data

--- client/src/components/Data.js
+++ client/src/components/Data.js
@@ -1,118 +1,153 @@
-import React from 'react';
+import React from "react";
 import { useSelector } from "react-redux";
 
-const VALVE_MAP = {1: "OPEN", 0: "CLOSED"};
+const VALVE_MAP = { 1: "OPEN", 0: "CLOSED" };
 
 const stylizeName = (name) => {
-    let spaced = name.split("_").join(" ");
-    let words = spaced.split(" ");
-    for(let idx in words){
-        words[idx] = words[idx][0].toUpperCase() + words[idx].slice(1);
-    }
-    return words.join(" ");
-}
+  let spaced = name.split("_").join(" ");
+  let words = spaced.split(" ");
+  for (let idx in words) {
+    words[idx] = words[idx][0].toUpperCase() + words[idx].slice(1);
+  }
+  return words.join(" ");
+};
 
 const Data = () => {
-    const data = useSelector((state) => {
-        return {
-            sensorState: state.data.sensorData,
-            valveState: state.data.valveData,
-            heartbeatState: state.data.general.heartbeat,
-            heartbeatStatus: state.data.general.heartbeat_status == undefined ? [] : [["", state.data.general.heartbeat_status]],
-            mode: state.data.general.mode
-        };
-    });
+  const data = useSelector((state) => {
+    return {
+      sensorState: state.data.sensorData,
+      valveState: state.data.valveData,
+      heartbeatState: state.data.general.heartbeat,
+      heartbeatStatus:
+        state.data.general.heartbeat_status == undefined
+          ? []
+          : [["", state.data.general.heartbeat_status]],
+      mode: state.data.general.mode,
+    };
+  });
 
-    const blockStyle = "m-1 p-4 bg-gray-100";
-    const blockHeaderStyle = "text-lg font-bold";
-    const groupHeaderStyle = "font-bold mb-1";
+  const blockStyle = "m-1 p-4 bg-gray-100";
+  const blockHeaderStyle = "text-lg font-bold";
+  const groupHeaderStyle = "font-bold mb-1";
 
-    const getLast = (arr) => (arr.length > 0 ? arr[arr.length - 1] : undefined);
-    const getColor = (status) => {
-        if (status.length === 0) {
-            return "black";
-        }
-        switch (status[status.length - 1][1]) {
-            case 3: return "green";
-            case 2: return "orange";
-            case 1: return "red";
-            default: return "blue";
-        }
+  const getLast = (arr) => (arr.length > 0 ? arr[arr.length - 1] : undefined);
+  const getColor = (status) => {
+    if (status.length === 0) {
+      return "black";
     }
-
-    const units = {
-        "thermocouple": "C",
-        "pressure": "PSI",
-        "load": "N",
-        "timestamp": "s"
+    switch (status[status.length - 1][1]) {
+      case 3:
+        return "green";
+      case 2:
+        return "orange";
+      case 1:
+        return "red";
+      default:
+        return "blue";
     }
+  };
 
-    return (
-        <center>
-            <h3 class={blockHeaderStyle}>Sensors</h3>
+  const units = {
+    thermocouple: "C",
+    pressure: "PSI",
+    load: "N",
+    timestamp: "s",
+  };
 
-            <div class={blockStyle}>
+  return (
+    <center>
+      <h3 class={blockHeaderStyle}>Sensors</h3>
+
+      <div class={blockStyle}>
+        <div>
+          {Object.keys(data.sensorState).map(
+            (sensor) =>
+              (sensor != "timestamp" && (
                 <div>
-                    {Object.keys(data.sensorState).map(sensor => (
-                        sensor != "timestamp" &&
-                        (<div>
-                            <h4 class={groupHeaderStyle}>{stylizeName(sensor)}</h4>
-                            <div>
-                                {Object.keys(data.sensorState[sensor]).map(loc => (
-                                    <p style={{ color: getColor(data.sensorState[sensor][loc]) }}>{stylizeName(loc)}: {getLast(data.sensorState[sensor][loc])} {units[sensor]}</p>
-                                ))}
-                            </div>
-                            <br></br>
-                        </div>) ||
-                        sensor === "timestamp" &&
-                        (<div>
-                            <h4 class={groupHeaderStyle}>Timestamp: {getLast(data.sensorState.timestamp)} s</h4>
-                        </div>)
+                  <h4 class={groupHeaderStyle}>{stylizeName(sensor)}</h4>
+                  <div className="font-mono">
+                    {Object.keys(data.sensorState[sensor]).map((loc) => (
+                      <p
+                        style={{
+                          color: getColor(data.sensorState[sensor][loc]),
+                        }}
+                      >
+                        {stylizeName(loc)}:{" "}
+                        {getLast(data.sensorState[sensor][loc])} {units[sensor]}
+                      </p>
                     ))}
+                  </div>
+                  <br></br>
                 </div>
-            </div>
+              )) ||
+              (sensor === "timestamp" && (
+                <div>
+                  <h4 class={groupHeaderStyle}>
+                    {"Timestamp: "}
+                    <span className="font-mono font-normal">
+                      {getLast(data.sensorState.timestamp)} s
+                    </span>
+                  </h4>
+                </div>
+              ))
+          )}
+        </div>
+      </div>
 
-            <br></br>
+      <br></br>
 
-            <h3 class={blockHeaderStyle}>Valves</h3>
+      <h3 class={blockHeaderStyle}>Valves</h3>
 
-            <div class={blockStyle}>
-            <div>
-                    {Object.keys(data.valveState).map(valve => (
-                        valve != "timestamp" &&
-                        (<div>
-                            <h4 class={groupHeaderStyle}>{stylizeName(valve)}</h4>
-                            <div>
-                                {Object.keys(data.valveState[valve]).map(loc => (
-                                    <p>{stylizeName(loc)}: {VALVE_MAP[data.valveState.solenoid[loc]]}</p>
-                                ))}
-                            </div>
-                            <br></br>
-                        </div>) ||
-                        valve === "timestamp" &&
-                        (<div>
-                            <h4 class={groupHeaderStyle}>Timestamp: {data.valveState.timestamp} s</h4>
-                        </div>)
+      <div class={blockStyle}>
+        <div>
+          {Object.keys(data.valveState).map(
+            (valve) =>
+              (valve != "timestamp" && (
+                <div>
+                  <h4 class={groupHeaderStyle}>{stylizeName(valve)}</h4>
+                  <div className="font-mono">
+                    {Object.keys(data.valveState[valve]).map((loc) => (
+                      <p>
+                        {stylizeName(loc)}:{" "}
+                        {VALVE_MAP[data.valveState.solenoid[loc]]}
+                      </p>
                     ))}
+                  </div>
+                  <br></br>
                 </div>
-            </div>
+              )) ||
+              (valve === "timestamp" && (
+                <div>
+                  <h4 class={groupHeaderStyle}>
+                    {"Timestamp: "}
+                    <span className="font-mono">
+                      {data.valveState.timestamp} s
+                    </span>
+                  </h4>
+                </div>
+              ))
+          )}
+        </div>
+      </div>
 
-            <br></br>
+      <br></br>
 
-            <h3 class={blockHeaderStyle}>Heartbeat</h3>
-            <div class={blockStyle}>
-                <p style={{ color: getColor(data.heartbeatStatus) }}> Timestamp: {data.heartbeatState}  </p>
-            </div>
+      <h3 class={blockHeaderStyle}>Heartbeat</h3>
+      <div class={blockStyle}>
+        <p style={{ color: getColor(data.heartbeatStatus) }}>
+          <span className="font-bold">{"Timestamp: "}</span>
+          <span className="font-mono">{data.heartbeatState}s</span>
+        </p>
+      </div>
 
-            <br></br>
+      <br></br>
 
-            <h3 class={blockHeaderStyle}>Mode</h3>
-            <div class={blockStyle}>
-                <p>{data.mode}  </p>
-            </div>
-
-        </center>
-    );
-}
+      <h3 class={blockHeaderStyle}>Mode</h3>
+      <div class={blockStyle}>
+        <p>{data.mode} </p>
+      </div>
+    </center>
+  );
+};
 
 export default Data;
