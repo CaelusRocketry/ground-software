a0e8bfe089931a7881f66cfdabe8ed6a3903bf1e
Aditya Vasantharao <hsfan612@gmail.com>
2020-10-15 00:48:54 -0400

    Add CLI to GS server

--- server/main.py
+++ server/main.py
@@ -9,11 +9,36 @@ from flask_socketio import SocketIO, emit, Namespace
 import time
 import logging
 
+import argparse
+
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
-config = json.loads(open("config.json").read())
+config = {}
+
+parser = argparse.ArgumentParser(description='Run the Project Caelus Ground Station Server.', formatter_class=argparse.RawTextHelpFormatter)
+
+parser.add_argument('--config', help="The config file to use for the simulation (enter " + 
+        "local if you want to run the simulation on the default local config). \n" + 
+        "Default: config.json")
+
+args = parser.parse_args()
+
+if args.config == "local":
+    config = json.loads(open("config.json").read())
+    config["GS_IP"] = "127.0.0.1"
+    config["SOCKETIO_HOST"] = "127.0.0.1"
+elif args.config != None:
+    try:
+        config = json.loads(open(args.config).read())
+    except:
+        raise Exception("Error reading from config file '" + args.config + "'")
+else:
+    config = json.loads(open("config.json").read())
+
+
+
 GS_IP = config["telemetry"]["GS_IP"]
 GS_PORT = config["telemetry"]["GS_PORT"]
 
