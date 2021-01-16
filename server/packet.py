f072ad4bc72adcfc59cefd2e31551a12cdadfd2c
HakkaNoodles <akundiharika@gmail.com>
2021-01-15 23:36:39 -0500

    Made header from an object into a string

--- server/packet.py
+++ server/packet.py
@@ -15,7 +15,7 @@ class LogPriority(IntEnum):
 class Log:
     """ Log class stores messages to be sent to and from ground and flight station """
 
-    def __init__(self, header={}, message={}, timestamp: float = time.time()):
+    def __init__(self, header, message={}, timestamp: float = time.time()):
         self.header = header
         self.message = message
         self.timestamp = timestamp
