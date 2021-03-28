ad8ae0a82560d8ec57e283f8e96451b77707f767
Suraj Vaddi <surajnvaddi@gmail.com>
2021-03-28 18:00:47 -0400

    Add path to footer

--- client/src/views/StageView.js
+++ client/src/views/StageView.js
@@ -1,17 +1,14 @@
 import React from "react";
 import SplitPane from "react-split-pane";
-
-import ButtonPane from "../panes/ButtonPane";
 import SequencePane from "../panes/SequencePane";
 
 import ChecklistPane from "../panes/ChecklistPane";
 
 const StageView = () => (
-  <><SplitPane className="view" split="vertical" defaultSize="50%">  
+  <SplitPane className="view" split="vertical" defaultSize="50%">  
     <SequencePane />
     <ChecklistPane />
   </SplitPane>
-  <FooterPane /></>
 );
 
 export default StageView;
