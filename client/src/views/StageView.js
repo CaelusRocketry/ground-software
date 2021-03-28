0e29578620ef8eef694f5830fbc163a170e52f97
Suraj Vaddi <surajnvaddi@gmail.com>
2021-03-28 17:30:56 -0400

    Add footer pane to views

--- client/src/views/StageView.js
+++ client/src/views/StageView.js
@@ -1,9 +1,8 @@
 import React from "react";
 import SplitPane from "react-split-pane";
 
-import ButtonPane from "../panes/ButtonPane";
 import SequencePane from "../panes/SequencePane";
-
+import FooterPane from "../panes/FooterPane"
 import ChecklistPane from "../panes/ChecklistPane";
 
 const StageView = () => (
