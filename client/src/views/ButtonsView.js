c706de540794058edafc91d2572b7ecefb4e753f
HakkaNoodles <akundiharika@gmail.com>
2021-03-29 19:14:12 -0400

    Changes comments

--- client/src/views/ButtonsView.js
+++ client/src/views/ButtonsView.js
@@ -6,7 +6,6 @@ import ButtonPane from "../panes/ButtonPane.tsx";
 
 const ButtonsView = () => (
   <><SplitPane className="view" split="vertical" defaultSize="50%">  
-    //Notifications Bar (BlockHeader??)
     //From array of valves, create "button bar"
       //OPEN/CLOSE/PRIORITY DROPDOWN -> onClickedActuateValve(valveLocation?: string, valveType?: string, actuationType?: string, actuationPriority?: string)
       //Pulse??
@@ -15,8 +14,6 @@ const ButtonsView = () => (
 )
   </SplitPane>
   <FooterPane /></>
-
-  //Define Buttons
       
 );
 
