d64add252327f7fba568c6796ed0681e1d08f414
HakkaNoodles <akundiharika@gmail.com>
2021-03-29 20:33:35 -0400

    ButtonBar setup

---
+++ client/src/panes/ButtonBarPane.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ButtonBar = () => (
+    
+    //1 huge div with everything
+    //4 small divs with NCSV-1, NCSV-2, NOSV-1, NOSV-2
+    //Look through arrays
+
+);
+
+export default ButtonBar;
