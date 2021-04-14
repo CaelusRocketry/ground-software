d794295b774011cca1aebc45fa0eb116ccfcc9b3
ani131 <anish.paspuleti@gmail.com>
2021-04-13 21:39:00 -0400

    Add localstorage for checklists

--- client/src/panes/ChecklistPane.tsx
+++ client/src/panes/ChecklistPane.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import { Checkbox, useCheckboxState } from 'pretty-checkbox-react';
 import styled from 'styled-components';
 
@@ -31,27 +31,47 @@ const stageParameters = [
 var x = 0;
 var y = 0;
 
+var arr = [ "false", "false", "false", "false", "false"];
 
 const ChecklistPane = () => {
   const currentStage = useSelector((state: CaelusState) =>
-  stages.indexOf(state.data.general.stage)
+    stages.indexOf(state.data.general.stage)
   );
-  
+
   function nextButtonPushed() {
     if (x < 3) {
       x = x + 1;
     }
   }
-  
+
   function backButtonPushed() {
     if (x > 0) {
       x = x - 1;
     }
   }
-  
-  const checkbox = useCheckboxState();
-  
-  var firstChecked = false;
+
+  // handleFormSubmit = () => {
+  //   const { user, rememberMe } = state;
+  //   localStorage.setItem('rememberMe', rememberMe);
+  //   localStorage.setItem('user', rememberMe ? user : '');
+  // };
+
+  // const checkbox = useCheckboxState();
+
+    
+
+  // const [checkboxState, setCheckboxState] = useState([ "false", "false", "false" ]);   
+
+  const handleCheckbox = () => {
+
+    localStorage.setItem("check1", JSON.stringify(true) ? JSON.stringify(false) : JSON.stringify(true));
+    console.log(localStorage.getItem("check1"));
+
+  }
+
+  localStorage.setItem("check1", "false")
+  localStorage.setItem("check2", "false")
+
 
   return (
     <div
@@ -60,52 +80,111 @@ const ChecklistPane = () => {
     >
       <Header>Checklist: {stageNames[stages[x]]}</Header>
       <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-        <Button onClick={backButtonPushed}> Back </Button> 
-        <Button disabled onClick={() => window.location.reload()}>{stageNames[stages[x + 1]]}</Button> 
+        <Button onClick={backButtonPushed}> Back </Button>
+        <Button disabled onClick={() => window.location.reload()}>{stageNames[stages[x + 1]]}</Button>
         <Button onClick={nextButtonPushed}> Next </Button>
       </div>
-        {(stageNames[stages[x]] === stageNames[stages[currentStage]]) 
-          ?Object.entries(stageParameters[x]).map(([num, step]) => {
-            return(
-              <Checkbox //locked
-                // onClick={checkbox.setState(true)}
-                
-                style={{ fontSize: "20px", marginBottom: 20, marginTop: 8, marginLeft: 20}}
-                animation="jelly" 
-                // icon=() add icon if you want
-                color='warning'
-                shape="round"
-                
-              >
-                {step}
-              </Checkbox>
-            )
-          })
-          :Object.entries(stageParameters[x]).map(([num, step]) => {
+    
+      <input 
+        className="Question__answer-checkbox"  
+        onChange={() => {
+          localStorage.setItem("check1", localStorage.getItem("check1") === "false" ? "true" : "false" );
+          console.log("1- " + localStorage.getItem("check1"));
+        }}
+        type="checkbox"
+        style={{ fontSize: "20px", marginBottom: 20, marginTop: 8, marginLeft: 20 }}
+      /> 
 
-            //var
-            return(
-              <Checkbox disabled checked
+      {/* <input 
+        className="Question__answer-checkbox"  
+        onClick={() => {
+          localStorage.setItem("check2", localStorage.getItem("check2") === "false" ? "true" : "false" );
+          console.log("2- " + localStorage.getItem("check2"));
+        }}
+        type="checkbox"
+        style={{ fontSize: "20px", marginBottom: 20, marginTop: 8, marginLeft: 20 }}
+      />  */}
 
-                onClick={() => firstChecked = true}
+      {/* var storedNames = JSON.parse(localStorage.getItem("names")); */}
 
-                  
 
-                // onChange={() => checkbox.setState(false)}
-                // defaultValue=""
-                style={{ fontSize: "20px", marginBottom: 20, marginTop: 8, marginLeft: 20}}
-                animation="jelly" 
-                // icon=() add icon if you want
-                shape="round"
-              
+      
 
-              >
-                {step}
-              </Checkbox>
-            )
-          })
-        }
-      </div>
+
+      {/* <input 
+        className="Question__answer-checkbox"
+        onClick={() => {
+          console.log("checkbox stored" + checkboxState);
+          localStorage.setItem("true", checkboxState[0]);
+          setCheckboxState(checkboxState[0] === "true" ? arr => [] : "true" );
+        }}
+        type="checkbox"
+        style={{ fontSize: "20px", marginBottom: 20, marginTop: 8, marginLeft: 20 }}
+      /> */}
+
+
+      {/* {(stageNames[stages[x]] === stageNames[stages[currentStage]])
+        ? Object.entries(stageParameters[x]).map(([num, step]) => {
+          localStorage.setItem('check', "0")
+          return (
+            <input //locked
+              // onClick={checkbox.setState(true)}
+              className="Question__answer-checkbox"
+              // checked={true}localStorage.setItem('rememberMe', rememberMe);
+
+              onClick={() => {
+
+                localStorage.setItem("true", checkboxState);
+                console.log("checkbox stored" + checkboxState);
+              }}
+
+
+
+              type="checkbox"
+              value={step}
+              // onChange={this.props.setAnswer}
+              //defaultChecked={ }
+              style={{ fontSize: "20px", marginBottom: 20, marginTop: 8, marginLeft: 20 }}
+            // animation="jelly" 
+            // icon=() add icon if you want
+            // color='red'
+            // shape="round"
+
+            />
+
+          )
+        })
+        : Object.entries(stageParameters[x]).map(([num, step]) => {
+
+          //var
+          return (
+            <Checkbox disabled checked
+
+              onClick={() => {
+                firstChecked = true
+
+                localStorage.setItem('first', firstChecked.toString());
+
+              }}
+
+
+
+
+              // onChange={() => checkbox.setState(false)}
+              // defaultValue=""
+              style={{ fontSize: "20px", marginBottom: 20, marginTop: 8, marginLeft: 20 }}
+              animation="jelly"
+              // icon=() add icon if you want
+              shape="round"
+
+
+            >
+              {step}
+            </Checkbox>
+          )
+        }) 
+      } */}
+    </div>
 
   );
 };
