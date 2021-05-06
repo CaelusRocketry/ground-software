3550caf87130eec3aeb2a85534ea873fd50d52ca
Suraj Vaddi <surajnvaddi@gmail.com>
2021-05-06 19:38:03 -0400

    Update livestream coords

--- client/src/lib/livestreampid.js
+++ client/src/lib/livestreampid.js
@@ -1,6 +1,6 @@
 import Nitrouspid from "../images/livestream/LivestreamBaseNitrous.png";
 import Ethanolpid from "../images/livestream/LivestreamBaseEthanol.png";
-import Fullpid from "../images/livestream/LivestreamBaseFull.png";
+//import Fullpid from "../images/livestream/LivestreamBaseFull.png";
 
 // CONSTANTS
 export const VALVE_MAP = {
@@ -19,52 +19,52 @@ export const PADDING = {
     },
     "COORDINATES" : {
       "LABELS" : {
-        "HEADER": {
-          "TEXT" : {
-            x1: (0/width) * 100 + '%',
-            y1: (0/height) * 100 + '%',
-            x2: (width/width) * 100 + '%',
-            y2: (160/height) * 100 + '%'
-          }
-        },
+        // "HEADER": {
+        //   "TEXT" : {
+        //     x1: (0/width) * 100 + '%',
+        //     y1: (0/height) * 100 + '%',
+        //     x2: (width/width) * 100 + '%',
+        //     y2: (160/height) * 100 + '%'
+        //   }
+        // },
         "NITROUS": {
           "TEXT" : {
             x1: (96/width) * 100 + '%',
             y1: (246/height) * 100 + '%',
-            x2: (1015/width) * 100 + '%',
-            y2: (349/height) * 100 + '%'
+            x2: ((width-1015)/width) * 100 + '%',
+            y2: ((height-349)/height) * 100 + '%'
           }
         },
         "APHLEX": {
           "TEXT" : {
             x1: (1775/width) * 100 + '%',
             y1: (246/height) * 100 + '%',
-            x2: (2694/width) * 100 + '%',
-            y2: (349/height) * 100 + '%'
+            x2: ((width-2694)/width) * 100 + '%',
+            y2: ((height-349)/height) * 100 + '%'
           }
         },
         "VENT": {
           "TEXT" : {
             x1: (375/width) * 100 + '%',
             y1: (418/height) * 100 + '%',
-            x2: (503/width) * 100 + '%',
-            y2: (466/height) * 100 + '%'
+            x2: ((width-503)/width) * 100 + '%',
+            y2: ((height-466)/height) * 100 + '%'
           }
         },
         "N2OFLIGHT": {
           "TEXT" : {
             x1: (642/width) * 100 + '%',
             y1: (614/height) * 100 + '%',
-            x2: (874/width) * 100 + '%',
-            y2: (1184/height) * 100 + '%'
+            x2: ((width-874)/width) * 100 + '%',
+            y2: ((height-1184)/height) * 100 + '%'
           }
         },
         "N2OSTORAGE": {
           "TEXT" : {
             x1: (134/width) * 100 + '%',
             y1: (827/height) * 100 + '%',
-            x2: (373/width) * 100 + '%',
-            y2: (1486/height) * 100 + '%'
+            x2: ((width-373)/width) * 100 + '%',
+            y2: ((height-1486)/height) * 100 + '%'
           }
         },
       },
@@ -73,41 +73,41 @@ export const PADDING = {
           "NITROUS_ISOLATED": {
             "TEXT" : {
               x1: (132/width) * 100 + '%',
-              y1: (711/width) * 100 + '%',
-              x2: (401/height) * 100 + '%',
-              y2: (812/height) * 100 + '%'
+              y1: (711/height) * 100 + '%',
+              x2: ((width-401)/width) * 100 + '%',
+              y2: ((height-812)/height) * 100 + '%'
             }
           },
           "NITROUS_TOP": {
             "TEXT" : {
               x1: (633/width) * 100 + '%',
-              y1: (492/width) * 100 + '%',
-              x2: (904/height) * 100 + '%',
-              y2: (595/height) * 100 + '%'
+              y1: (492/height) * 100 + '%',
+              x2: ((width-904)/width) * 100 + '%',
+              y2: ((height-595)/height) * 100 + '%'
             }
           },
           "NITROUS_BOTTOM": {
             "TEXT" : {
               x1: (594/width) * 100 + '%',
-              y1: (1233/width) * 100 + '%',
-              x2: (886/height) * 100 + '%',
-              y2: (1319/height) * 100 + '%'
+              y1: (1233/height) * 100 + '%',
+              x2: ((width-886)/width) * 100 + '%',
+              y2: ((height-1319)/height) * 100 + '%'
             }
           },
           "TUBE": {
             "TEXT" : {
               x1: (1727/width) * 100 + '%',
-              y1: (1300/width) * 100 + '%',
-              x2: (2091/height) * 100 + '%',
-              y2: (1400/height) * 100 + '%'
+              y1: (1300/height) * 100 + '%',
+              x2: ((width-2091)/width) * 100 + '%',
+              y2: ((height-1400)/height) * 100 + '%'
             }
           },
           "INJECTOR": {
             "TEXT" : {
               x1: (1639/width) * 100 + '%',
-              y1: (688/width) * 100 + '%',
-              x2: (2000/height) * 100 + '%',
-              y2: (787/height) * 100 + '%'
+              y1: (688/height) * 100 + '%',
+              x2: ((width-2000)/width) * 100 + '%',
+              y2: ((height-787)/height) * 100 + '%'
             }
           },
         },
@@ -115,9 +115,9 @@ export const PADDING = {
           "ENGINE": {
             "TEXT" : {
               x1: (2067/width) * 100 + '%',
-              y1: (707/width) * 100 + '%',
-              x2: (2517/height) * 100 + '%',
-              y2: (817/height) * 100 + '%'
+              y1: (707/height) * 100 + '%',
+              x2: ((width-2517)/width) * 100 + '%',
+              y2: ((height-817)/height) * 100 + '%'
             }
           }
         }
@@ -126,25 +126,25 @@ export const PADDING = {
         "NITROUS_ISOLATED": {
           "TEXT" : {
             x1: (410/width) * 100 + '%',
-            y1: (722/width) * 100 + '%',
-            x2: (531/height) * 100 + '%',
-            y2: (842/height) * 100 + '%'
+            y1: (722/height) * 100 + '%',
+            x2: ((width-531)/width) * 100 + '%',
+            y2: ((height-842)/height) * 100 + '%'
           }
         },
         "NITROUS_TOP": {
           "TEXT" : {
             x1: (500/width) * 100 + '%',
-            y1: (448/width) * 100 + '%',
-            x2: (621/height) * 100 + '%',
-            y2: (570/height) * 100 + '%'
+            y1: (448/height) * 100 + '%',
+            x2: ((width-621)/width) * 100 + '%',
+            y2: ((height-570)/height) * 100 + '%'
           }
         },
         "NITROUS_BOTTOM": {
           "TEXT" : {
             x1: (885/width) * 100 + '%',
-            y1: (1323/width) * 100 + '%',
-            x2: (1004/height) * 100 + '%',
-            y2: (1445/height) * 100 + '%'
+            y1: (1323/height) * 100 + '%',
+            x2: ((width-1004)/width) * 100 + '%',
+            y2: ((height-1445)/height) * 100 + '%'
           }
         },
       }
--- client/src/panes/LivestreamPane.tsx
+++ client/src/panes/LivestreamPane.tsx
@@ -4,18 +4,155 @@ import Header from "../components/Header";
 import "../index.css";
 import { useSelector } from "react-redux";
 
-import { VALVE_MAP, PADDING } from "../lib/pid";
+import { VALVE_MAP, PADDING } from "../lib/livestreampid";
 import { CaelusState } from "../store/reducers";
 import config from "../config.json";
 import Livestreambase from "../images/livestream/LivestreamBaseNitrous.png";
+import { isReturnStatement } from "typescript";
 
-const LivestreamPane = () => (
-  <img
-  src={Livestreambase}
-  id={"livestreambase"}
-  alt={"livestreambase"}
-  style={{ width: "100%" }}
-/>
-);
+const LivestreamPane = () => {
+
+  const data = useSelector((state: CaelusState) => ({
+    sensorState: state.data.sensorData,
+    valveState: state.data.valveData,
+  }));
+
+  const getLast: <T>(arr: T[]) => T | undefined = (arr) =>
+    arr.length > 0 ? arr[arr.length - 1] : undefined;
+    
+  const round = (num: number | undefined, precision: number): number | undefined => {
+    let rounded = num ? Math.round(num * precision) / precision : undefined;
+    return rounded;
+  };
+
+  const sensorExists = (
+    sensorType: keyof typeof data.sensorState.sensors,
+    sensorLoc: string
+  ) =>
+    sensorType in data.sensorState.sensors &&
+    sensorLoc in data.sensorState.sensors[sensorType];
+  const valveExists = (valveLoc: string) =>
+    valveLoc in data.valveState.valves.solenoid;
+
+
+  return(
+  <div style={{ textAlign: "center" }}>
+      <select           
+        onChange={(e) => {
+          // @ts-expect-error
+          setDiagram({ type: e.target.value });
+        }}
+      >
+        <option value="NITROUS">{config.test_type.Nitrous}</option>
+        <option value="ETHANOL">{config.test_type.Ethanol}</option>
+        <option value="FULL">{config.test_type.Full}</option>
+      </select>
+
+      <div
+        style={{ position: "relative", textAlign: "center" }}
+        className="flexFont"
+      >
+        <img
+          src={Livestreambase}
+          id={"livestreambase"}
+          alt={"livestreambase"}
+          style={{ width: "100%" }}
+        />
+        {Object.entries(PADDING["NITROUS"]["COORDINATES"]["LABELS"]).map(([loc, label]) => (
+          <div
+            style={{
+              position: "absolute",
+              // // @ts-expect-error
+              left: label["TEXT"]["x1"],
+              // // @ts-expect-error
+              top: label["TEXT"]["y1"],
+              right: label["TEXT"]["x2"],
+              bottom: label["TEXT"]["y2"],
+              borderWidth: "2px",
+              borderColor: "#1835ab",
+            }}
+          >
+            <p style={{fontSize: "100%"}}
+              className={"diagram" + label}
+            >
+              {sensorExists("pressure", loc)
+                ? round(getLast(data.sensorState.sensors.pressure[loc])?.kalman, 1000)
+                : ""}
+            </p>
+          </div>
+ 
+        ))}
+        {Object.entries(PADDING["NITROUS"]["COORDINATES"]["SENSOR"]["PRESSURE"]).map(([loc, pressureSensor]) => (
+          <div 
+            style={{
+              position: "absolute",
+              left: pressureSensor["TEXT"]["x1"],
+              top: pressureSensor["TEXT"]["y1"],
+              right: pressureSensor["TEXT"]["x2"],
+              bottom: pressureSensor["TEXT"]["y2"],
+              borderWidth: "2px",
+              borderColor: "#8e0004",
+              fontSize: "10vw",
+              display: "inline",
+            }}
+          >
+            <p className={"diagram" + pressureSensor} style={{fontSize: "10px"}}>
+              {valveExists(loc)
+                ? VALVE_MAP[data.valveState.valves.solenoid[loc]]
+                : ""
+              }
+            </p>
+          </div>
+        ))}
+        {Object.entries(PADDING["NITROUS"]["COORDINATES"]["SENSOR"]["THRUST"]).map(([loc, thrustSensor]) => (
+          <div 
+            style={{
+              position: "absolute",
+              left: thrustSensor["TEXT"]["x1"],
+              top: thrustSensor["TEXT"]["y1"],
+              right: thrustSensor["TEXT"]["x2"],
+              bottom: thrustSensor["TEXT"]["y2"],
+              borderWidth: "2px",
+              borderColor: "#8e0004",
+              fontSize: "10vw",
+              display: "inline",
+            }}
+          >
+            <p className={"diagram" + thrustSensor} style={{fontSize: "10px"}}>
+              {valveExists(loc)
+                ? VALVE_MAP[data.valveState.valves.solenoid[loc]]
+                : ""
+              }
+            </p>
+          </div>
+        ))}
+        {Object.entries(PADDING["NITROUS"]["COORDINATES"]["VALVES"]).map(([loc, valve]) => (
+          <div 
+            style={{
+              position: "absolute",
+              left: valve["TEXT"]["x1"],
+              top: valve["TEXT"]["y1"],
+              right: valve["TEXT"]["x2"],
+              bottom: valve["TEXT"]["y2"],
+              borderWidth: "2px",
+              borderColor: "#8e0004",
+              fontSize: "10vw",
+              display: "inline",
+            }}
+          >
+            <p className={"diagram" + valve} style={{fontSize: "10px"}}>
+              {valveExists(loc)
+                ? VALVE_MAP[data.valveState.valves.solenoid[loc]]
+                : ""
+              }
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  
+
+  )
+};
 
 export default LivestreamPane;
